1aafec9f3d662c91e0cc9faac707f071685ce95f
Martin Stickley <mds1966@gmail.com>
2020-10-02 11:49:57 +0100

    Manually updated to use typescript

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,12 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+
+    siteName: 'Gridsome',
+    plugins: [
+      {
+        use: 'gridsome-plugin-typescript',
+      }
+    ]
+
 }
