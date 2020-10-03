b295140d746e4f333ce3fb6d4435b8c46d06acc5
Martin Stickley <mds1966@gmail.com>
2020-10-03 16:49:18 +0100

    Built type declaration files

---
+++ src/pages/About.vue.d.ts
@@ -0,0 +1,6 @@
+declare const _default: {
+    metaInfo: {
+        title: string;
+    };
+};
+export default _default;
--- src/utility.ts
+++ src/utility.ts
@@ -26,42 +26,42 @@ export function translateMouseCoordsToBoardCoords(
     return new Coord(cell_x, cell_y);
 }
  
-export class SystemColors {
-    static BLACK: string = '#000000';
-    static WHITE: string = '#FFFFFF';
-    static GREEN: string = '#90EE90';
-    static RED: string = '#FF0000';
-    static YELLOW: string = '#F5F527';
-    static ORANGE: string = '#FF4500';
-    static BLUE: string = '#0000FF';
-    static LIGHT_PURPLE: string = '#F0E3FE';
-}
-
-export class SystemKeys {
-
-    // Arrow Keys
-    static LEFT_ARROW: number = 37;
-    static UP_ARROW: number = 38;
-    static RIGHT_ARROW: number = 39;
-    static DOWN_ARROW: number = 40;
-
-    // Added VIM motion keys
-    static VIM_LEFT: number = 72;
-    static VIM_UP: number = 75;
-    static VIM_RIGHT: number = 76;
-    static VIM_DOWN: number = 74;
-
-    static ONE: number = 49;
-    static PAD_ONE: number = 97;
-    
-    static TWO: number = 50;
-    static PAD_TWO: number = 98;
-    
-    static THREE: number = 51;
-    static PAD_THREE: number = 99;
-    
-    static FOUR: number = 52;
-    static PAD_FOUR: number = 100;
-
-}
-
+// export class SystemColors {
+//     static BLACK: string = '#000000';
+//     static WHITE: string = '#FFFFFF';
+//     static GREEN: string = '#90EE90';
+//     static RED: string = '#FF0000';
+//     static YELLOW: string = '#F5F527';
+//     static ORANGE: string = '#FF4500';
+//     static BLUE: string = '#0000FF';
+//     static LIGHT_PURPLE: string = '#F0E3FE';
+// }
+// 
+// export class SystemKeys {
+// 
+//     // Arrow Keys
+//     static LEFT_ARROW: number = 37;
+//     static UP_ARROW: number = 38;
+//     static RIGHT_ARROW: number = 39;
+//     static DOWN_ARROW: number = 40;
+// 
+//     // Added VIM motion keys
+//     static VIM_LEFT: number = 72;
+//     static VIM_UP: number = 75;
+//     static VIM_RIGHT: number = 76;
+//     static VIM_DOWN: number = 74;
+// 
+//     static ONE: number = 49;
+//     static PAD_ONE: number = 97;
+//     
+//     static TWO: number = 50;
+//     static PAD_TWO: number = 98;
+//     
+//     static THREE: number = 51;
+//     static PAD_THREE: number = 99;
+//     
+//     static FOUR: number = 52;
+//     static PAD_FOUR: number = 100;
+// 
+// }
+// 
